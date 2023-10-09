1ce97391ac860c2266252edd90b0e0e2d8828862
jmarkfen <119049235+jmarkfen@users.noreply.github.com>
2023-10-09 12:33:42 +0800

    refactored barangay models

--- app/Models/Barangay.php
+++ app/Models/Barangay.php
@@ -2,13 +2,10 @@
 
 namespace App\Models;
 
-use Illuminate\Database\Eloquent\Factories\HasFactory;
-use Illuminate\Database\Eloquent\Model;
+use App\Models\Abstract\RecordModel;
 
-class Barangay extends Model
+class Barangay extends RecordModel
 {
-    use HasFactory;
-
     protected $fillable = [
         'key_id',
         'region',
@@ -17,33 +14,10 @@ class Barangay extends Model
         'barangay',
     ];
 
-    public function record_key()
+    protected static $keyModel = BarangayKey::class;
+
+    public function households()
     {
-        return $this->belongsTo(BarangayKey::class, 'key_id');
-    }
-
-    public function record_history()
-    {
-        return $this->hasMany(self::class, 'key_id', 'key_id');
-    }
-
-    public static function createWithNewKey(array $data): Barangay
-    {
-        $key = new BarangayKey();
-        $key->save();
-        $brgy = new Barangay($data);
-        $brgy->record_key()->associate($key);
-        $brgy->save();
-
-        return $brgy;
-    }
-
-    public function newRecord(array $data): Barangay
-    {
-        $brgy = new Barangay($data);
-        $brgy->record_key()->associate($this->record_key);
-        $brgy->save();
-
-        return $brgy;
+        return $this->hasMany(Household::class);
     }
 }
--- app/Models/BarangayKey.php
+++ app/Models/BarangayKey.php
@@ -2,26 +2,9 @@
 
 namespace App\Models;
 
-use Illuminate\Database\Eloquent\Builder;
-use Illuminate\Database\Eloquent\Factories\HasFactory;
-use Illuminate\Database\Eloquent\Model;
+use App\Models\Abstract\KeyModel;
 
-class BarangayKey extends Model
+class BarangayKey extends KeyModel
 {
-    use HasFactory;
-
-    public function records()
-    {
-        return $this->hasMany(Barangay::class, 'key_id');
-    }
-
-    public function latest_record()
-    {
-        return $this->records()->one()->ofMany();
-    }
-
-    public function scopeUnused(Builder $query)
-    {
-        return $query->has('records', '=', 0);
-    }
+    protected static $recordModel = Barangay::class;
 }
