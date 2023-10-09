5739ec33151666e130f51c0dbb0db45a1e0c3b45
jmarkfen <119049235+jmarkfen@users.noreply.github.com>
2023-10-09 12:33:33 +0800

    abstract models for keys and records

---
+++ app/Models/Abstract/KeyModel.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace App\Models\Abstract;
+
+use Illuminate\Database\Eloquent\Builder;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+abstract class KeyModel extends Model
+{
+    use HasFactory;
+
+    protected static $recordModel;
+
+    public function records()
+    {
+        return $this->hasMany(static::$recordModel, 'key_id');
+    }
+
+    public function latest_record()
+    {
+        return $this->records()->one()->ofMany();
+    }
+
+    public function scopeUnused(Builder $query)
+    {
+        return $query->has('records', '=', 0);
+    }
+}
