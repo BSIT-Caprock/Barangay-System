950f6bd00f49567840ffd542777e9dd7cefc32a8
jmarkfen <119049235+jmarkfen@users.noreply.github.com>
2023-10-17 15:05:37 +0800

    create `ResidentResource`
    fix #18

---
+++ app/Filament/Resources/ResidentResource.php
@@ -0,0 +1,71 @@
+<?php
+
+namespace App\Filament\Resources;
+
+use App\Filament\Resources\ResidentResource\Pages;
+use App\Filament\Resources\ResidentResource\RelationManagers;
+use App\Models\Resident;
+use Filament\Forms;
+use Filament\Forms\Form;
+use Filament\Resources\Resource;
+use Filament\Tables;
+use Filament\Tables\Table;
+use Illuminate\Database\Eloquent\Builder;
+use Illuminate\Database\Eloquent\SoftDeletingScope;
+
+class ResidentResource extends Resource
+{
+    protected static ?string $model = Resident::class;
+
+    protected static ?string $navigationIcon = 'heroicon-o-rectangle-stack';
+
+    protected static ?string $recordTitleAttribute = 'latest_record.full_name';
+
+    public static function form(Form $form): Form
+    {
+        return $form
+            ->schema([
+                //
+            ]);
+    }
+
+    public static function table(Table $table): Table
+    {
+        return $table
+            ->columns([
+                Tables\Columns\TextColumn::make('latest_record.full_name')->label('Name'),
+                Tables\Columns\TextColumn::make('latest_record.address')->label('Address'),
+                Tables\Columns\TextColumn::make('latest_record.age')->label('Age'),
+            ])
+            ->filters([
+                //
+            ])
+            ->actions([
+                Tables\Actions\EditAction::make(),
+            ])
+            ->bulkActions([
+                Tables\Actions\BulkActionGroup::make([
+                    Tables\Actions\DeleteBulkAction::make(),
+                ]),
+            ])
+            ->emptyStateActions([
+                Tables\Actions\CreateAction::make(),
+            ]);
+    }
+    
+    public static function getRelations(): array
+    {
+        return [
+            //
+        ];
+    }
+    
+    public static function getPages(): array
+    {
+        return [
+            'index' => Pages\ListResidents::route('/'),
+            'create' => Pages\CreateResident::route('/create'),
+            'edit' => Pages\EditResident::route('/{record}/edit'),
+        ];
+    }    
+}
