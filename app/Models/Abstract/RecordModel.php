6174a3c4fda0a9731bcbfa4c30dd65ee28532369
jmarkfen <119049235+jmarkfen@users.noreply.github.com>
2023-10-21 13:50:12 +0800

    Update RecordModel.php

--- app/Models/Abstract/RecordModel.php
+++ app/Models/Abstract/RecordModel.php
@@ -15,35 +15,4 @@ abstract class RecordModel extends Model
     {
         return $this->belongsTo(static::$keyModel, 'key_id');
     }
-
-    public function record_history()
-    {
-        return $this->hasMany(static::class, 'key_id', 'key_id');
-    }
-
-    public function latest_record()
-    {
-        return $this->record_history()->one()->ofMany();
-    }
-
-    public static function createWithNewKey(array $data)
-    {
-        // Create a new key model and associate it with the record
-        $key = static::$keyModel::create();
-        $key->save();
-        $record = new static($data);
-        $record->record_key()->associate($key);
-        $record->save();
-
-        return $record;
-    }
-
-    public function newRecord(array $data)
-    {
-        $record = new static($data);
-        $record->record_key()->associate($this->record_key);
-        $record->save();
-
-        return $record;
-    }
 }
