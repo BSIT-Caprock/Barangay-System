3a6ef765add3480e4092ed32e5ede0f605747f0a
jmarkfen <119049235+jmarkfen@users.noreply.github.com>
2023-10-27 14:17:17 +0800

    added street and zone attribute

--- app/Models/Locations/House.php
+++ app/Models/Locations/House.php
@@ -3,6 +3,7 @@
 namespace App\Models\Locations;
 
 use App\Models\Abstract\SelfReferenceTrait;
+use Illuminate\Database\Eloquent\Casts\Attribute;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
@@ -29,5 +30,12 @@ class House extends Model
     public function street()
     {
         return $this->belongsTo(Street::class);
+    }
+
+    public function streetAndZone(): Attribute
+    {
+        return Attribute::make(
+            get: fn (mixed $value) => $this->street->street . ', ' . $this->zone->zone,
+        );
     }
 }
