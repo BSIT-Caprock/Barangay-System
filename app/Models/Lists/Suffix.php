45242c562c848eb2a95b38ccf1175c991bcfc5bb
jmarkfen <119049235+jmarkfen@users.noreply.github.com>
2023-10-27 08:15:25 +0800

    Create Suffix.php

---
+++ app/Models/Lists/Suffix.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Models\Lists;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Suffix extends Model
+{
+    use HasFactory;
+
+    protected $fillable = ['suffix'];
+}
