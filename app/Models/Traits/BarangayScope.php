13ab6f2e74e7a854edceeed0181059023f3b4b11
jmarkfen <119049235+jmarkfen@users.noreply.github.com>
2023-10-27 14:32:11 +0800

    fixed namespace

--- app/Models/Traits/BarangayScope.php
+++ app/Models/Traits/BarangayScope.php
@@ -2,13 +2,18 @@
 
 namespace App\Models\Traits;
 
-use App\Models\Barangays\BarangayRecord;
+use App\Models\Locations\BarangayRecord;
 use Illuminate\Database\Eloquent\Builder;
 
+/**
+ * model must have a 'barangay' relationship
+ */
 trait BarangayScope
 {
+    
     public function scopeOfBarangayRecord(Builder $query, BarangayRecord $barangayRecord)
     {
+        // where 'barangay' has 'id' equal to $barangayRecord->id
         return $query->whereHas('barangay', function (Builder $query) use ($barangayRecord) {
             $query->where('id', $barangayRecord->id);
         });
