5fa32562db0ff75fd68127d9126d8828a82ef64e
jmarkfen <119049235+jmarkfen@users.noreply.github.com>
2023-10-27 08:15:03 +0800

    move `Occupation`

---
+++ app/Models/Lists/Occupation.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Models\Lists;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Occupation extends Model
+{
+    use HasFactory;
+
+    protected $fillable = ['occupation'];
+}
