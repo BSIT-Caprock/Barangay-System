37b8e51ba0ebe3d3d5cbaecedf27505a5225d2f2
jmarkfen <119049235+jmarkfen@users.noreply.github.com>
2023-11-06 20:27:53 +0800

    setup role/permission-based scopes and policies

--- app/Models/Scopes/BarangayScope.php
+++ app/Models/Scopes/BarangayScope.php
@@ -2,6 +2,7 @@
 
 namespace App\Models\Scopes;
 
+use App\Models\User;
 use Auth;
 use Illuminate\Database\Eloquent\Builder;
 use Illuminate\Database\Eloquent\Model;
@@ -14,10 +15,21 @@ class BarangayScope implements Scope
      */
     public function apply(Builder $builder, Model $model): void
     {
-        $user = Auth::user(); // Assuming the user is authenticated
-        // user with no barangay id is usually a super admin
+         // Assuming the user is authenticated
+         /** @var User */
+        $user = Auth::user();
+        
+        if ($user->hasRole('Superadministrator')) {
+            // Superadministrators can access all models
+            return;
+        }
+
         if ($user->barangay_id) {
             $builder->where('barangay_id', $user->barangay_id);
+        } else {
+            // If user doesn't have a barangay_id, restrict access to no models
+            $builder->where('barangay_id', 0);
         }
+        
     }
 }
---
+++ database/seeders/RolesAndPermissionsSeeder.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+use Spatie\Permission\Models\Permission;
+use Spatie\Permission\Models\Role;
+
+class RolesAndPermissionsSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        // Reset cached roles and permissions
+        app()[\Spatie\Permission\PermissionRegistrar::class]->forgetCachedPermissions();
+        // barangay users
+        Permission::create(['name' => 'list users']);
+        Permission::create(['name' => 'view users']);
+        Permission::create(['name' => 'add users']);
+        Permission::create(['name' => 'edit users']);
+        Permission::create(['name' => 'delete users']);
+        // barangay zones
+        // barangay streets
+        // barangay households
+        // barangay residents
+        // barangay resident history
+        // documents templates
+        // document requests
+        // barangay personnel
+        Role::create(['name' => 'Superadministrator']);
+        Role::create(['name' => 'Barangay Administrator']);
+        Role::create(['name' => 'Punong Barangay']);
+        Role::create(['name' => 'Barangay Secretary']);
+        Role::create(['name' => 'Barangay Kagawad']);
+        Role::create(['name' => 'Barangay Treasurer']);
+    }
+}
