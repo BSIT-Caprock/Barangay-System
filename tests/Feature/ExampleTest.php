b34413f2f57a0af706870abb1a4ff5a7ee572a2a
jmarkfen <119049235+jmarkfen@users.noreply.github.com>
2023-11-06 14:55:36 +0800

    Update ExampleTest.php

--- tests/Feature/ExampleTest.php
+++ tests/Feature/ExampleTest.php
@@ -13,22 +13,3 @@ it('renders successfully', function () {
     $response->assertSuccessful();
     $response->assertSeeLivewire(Login::class);
 });
-
-test('livewire has method', function () {
-    $lw = livewire(ListResidents::class);
-    // $hasMethod = method_exists($lw, 'assertCanSeeTableRecords');
-    // expect($hasMethod)->toBe(true);
-    livewire(ListResidents::class)->assertCanSeeTableRecords();
-});
-
-
-test('123', function () {
-    $cls = Barangay::class;
-    $record = new BarangayRecord;
-    dump($cls::getRecordModel());
-    dump($cls::getForeignId());
-    dump($record->getFillable());
-    dump($record->entity()->getRelated());
-});
-
-test('test hello');
