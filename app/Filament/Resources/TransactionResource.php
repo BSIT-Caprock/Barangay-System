ca0cc526d89f817e4274c60a8e13786059f201b5
hzlpntl <hazelrosemarie.pontilla@evsu.edu.ph>
2023-11-09 10:13:26 +0800

    edit

--- app/Filament/Resources/TransactionResource.php
+++ app/Filament/Resources/TransactionResource.php
@@ -19,7 +19,7 @@ class TransactionResource extends Resource
 {
     protected static ?string $model = Transaction::class;
 
-    protected static ?string $navigationIcon = 'heroicon-o-rectangle-stack';
+    protected static ?string $navigationIcon = 'heroicon-o-inbox';
 
     protected static ?string $navigationLabel = 'Transaction Logs';
 
