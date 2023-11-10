295b3635c2a62c4b20304fb0d9ca15743f105ba9
hzlpntl <hazelrosemarie.pontilla@evsu.edu.ph>
2023-11-10 15:25:05 +0800

    latest update

--- app/Filament/Widgets/StatsOverview.php
+++ app/Filament/Widgets/StatsOverview.php
@@ -3,7 +3,6 @@
 namespace App\Filament\Widgets;
 
 use App\Models\Reports;
-use App\Models\Transaction;
 use Filament\Widgets\StatsOverviewWidget as BaseWidget;
 use Filament\Widgets\StatsOverviewWidget\Stat;
 
@@ -18,11 +17,7 @@ class StatsOverview extends BaseWidget
             ->color('primary')
             ->chart([2,5,8,3,5,2,6]),
 
-            Stat::make('Total Transaction Logs', Transaction::count())
-            ->description('')
-            ->descriptionIcon('heroicon-s-arrow-trending-up')
-            ->color('success')
-            ->chart([5,2,7,3,9,4,6,])
+            
         ];
     }
 }
