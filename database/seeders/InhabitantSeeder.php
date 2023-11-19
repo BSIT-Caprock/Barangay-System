8e2ddd9d94f4fe3a4b56a84f1126a64390b32741
jmarkfen <119049235+jmarkfen@users.noreply.github.com>
2023-11-19 16:02:45 +0800

    rename seeder

--- database/seeders/InhabitantSeeder.php
+++ database/seeders/InhabitantSeeder.php
@@ -5,7 +5,7 @@ namespace Database\Seeders;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 
-class ResidentSeeder extends Seeder
+class InhabitantSeeder extends Seeder
 {
     /**
      * Run the database seeds.
