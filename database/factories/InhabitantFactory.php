cc5dba23caccaab7605236529d4bab5ff40c1e70
jmarkfen <119049235+jmarkfen@users.noreply.github.com>
2023-11-19 15:59:37 +0800

    Create InhabitantFactory.php

---
+++ database/factories/InhabitantFactory.php
@@ -0,0 +1,49 @@
+<?php
+
+namespace Database\Factories;
+
+use App\Models\Sex;
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+/**
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Inhabitant>
+ */
+class InhabitantFactory extends Factory
+{
+    /**
+     * Define the model's default state.
+     *
+     * @return array<string, mixed>
+     */
+    public function definition(): array
+    {
+        $sexId = fake()->randomElement([
+            Sex::MALE,
+            Sex::FEMALE,
+        ]);
+
+        $gender = [
+            Sex::MALE => 'male',
+            Sex::FEMALE => 'female',
+        ][$sexId];
+
+        return [
+            'barangay_id' => 1,
+            'household_id' => null,
+            'last_name' => fake()->lastName(),
+            'first_name' => fake()->firstName($gender),
+            'middle_name' => fake()->lastName(),
+            'extension_name' => fake()->suffix(),
+            'house_id' => null,
+            'street_id' => null,
+            'zone_id' => null,
+            'birth_place_id' => null,
+            'birth_date' => fake()->date(),
+            'sex_id' => $sexId,
+            'civil_status_id' => null,
+            'citizenship_id' => null,
+            'occupation_id' => null,
+            'date_accomplished' => fake()->date(),
+        ];
+    }
+}
