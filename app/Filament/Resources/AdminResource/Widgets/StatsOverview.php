e3038d835929bcedca67e6bb9280d0f35bbf0915
hzlpntl <hazelrosemarie.pontilla@evsu.edu.ph>
2023-11-23 05:14:16 +0800

    commenting the widgets

--- app/Filament/Resources/AdminResource/Widgets/StatsOverview.php
+++ app/Filament/Resources/AdminResource/Widgets/StatsOverview.php
@@ -7,30 +7,30 @@ use Filament\Widgets\StatsOverviewWidget\Stat;
 
 class StatsOverview extends BaseWidget
 {
-    protected static ?string $pollingInterval = '15s';
+    // protected static ?string $pollingInterval = '15s';
 
     protected function getStats(): array
     {
         return [
-            // Stat::make('Total Residents', Resident::count())
-            Stat::make('Total Residents', 108)
-            // change the value of other stats if there's a data added
-            ->description('Increased residents as of now')
-            ->descriptionIcon('heroicon-m-arrow-trending-up')
-            ->color('success')
-            ->chart([7, 3, 4, 5, 6, 3, 5, 9]),
+            // // Stat::make('Total Residents', Resident::count())
+            // Stat::make('Total Residents', 108)
+            // // change the value of other stats if there's a data added
+            // ->description('Increased residents as of now')
+            // ->descriptionIcon('heroicon-m-arrow-trending-up')
+            // ->color('success')
+            // ->chart([7, 3, 4, 5, 6, 3, 5, 9]),
 
-            Stat::make('Total Barangay', 37)
-            ->description('Updated barangay-data as of now')
-            ->descriptionIcon('heroicon-m-arrow-trending-up')
-            ->color('primary')
-            ->chart([7, 4, 5, 7, 3, 5, 8, 10]),
+            // Stat::make('Total Barangay', 37)
+            // ->description('Updated barangay-data as of now')
+            // ->descriptionIcon('heroicon-m-arrow-trending-up')
+            // ->color('primary')
+            // ->chart([7, 4, 5, 7, 3, 5, 8, 10]),
 
-            Stat::make('Total Document Requested', 50)
-            ->description('Updated document-requested as of now')
-            ->descriptionIcon('heroicon-m-arrow-trending-up')
-            ->color('danger')
-            ->chart([7, 4, 3, 5, 3, 5, 6, 10]),
+            // Stat::make('Total Document Requested', 50)
+            // ->description('Updated document-requested as of now')
+            // ->descriptionIcon('heroicon-m-arrow-trending-up')
+            // ->color('danger')
+            // ->chart([7, 4, 3, 5, 3, 5, 6, 10]),
 
     
 
