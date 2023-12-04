73214723852026815a796fdd412fd6ed44ddc3d1
jmarkfen <119049235+jmarkfen@users.noreply.github.com>
2023-12-04 21:25:13 +0800

    sort

--- app/Providers/Filament/AdminPanelProvider.php
+++ app/Providers/Filament/AdminPanelProvider.php
@@ -4,6 +4,8 @@ namespace App\Providers\Filament;
 
 use App\Filament\Resources\HouseholdResource\Widgets\HouseholdCount;
 use App\Filament\Resources\InhabitantResource\Widgets\TotalInhabitants;
+use App\Filament\Widgets\CivilStatusChart;
+use App\Filament\Widgets\SexPopulationChart;
 use Filament\Http\Middleware\Authenticate;
 use Filament\Http\Middleware\DisableBladeIconComponents;
 use Filament\Http\Middleware\DispatchServingFilamentEvent;
@@ -74,10 +76,10 @@ class AdminPanelProvider extends PanelProvider
             ->pages([
                 Pages\Dashboard::class,
             ])
-            ->discoverWidgets(in: app_path('Filament/Widgets'), for: 'App\\Filament\\Widgets')
+            // ->discoverWidgets(in: app_path('Filament/Widgets'), for: 'App\\Filament\\Widgets')
             ->widgets([
-                Widgets\AccountWidget::class,
-                Widgets\FilamentInfoWidget::class,
+                CivilStatusChart::class,
+                SexPopulationChart::class,
                 TotalInhabitants::class,
                 HouseholdCount::class,
             ])
