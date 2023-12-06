939c7b44a652168d3bda2f06daf429923f81bc8f
Jaymark Feniza <119049235+jmarkfen@users.noreply.github.com>
2023-12-06 13:30:09 +0800

    Update families (#124)
    Co-authored-by: jmarkfen <jmarkfen@users.noreply.github.com>

--- app/Filament/Resources/FamilyResource.php
+++ app/Filament/Resources/FamilyResource.php
@@ -31,45 +31,63 @@ class FamilyResource extends Resource
     public static function form(Form $form): Form
     {
         return $form
-            ->schema([
-                Forms\Components\MorphToSelect::make('location')->types([
-                    Forms\Components\MorphToSelect\Type::make(Zone::class)
-                        ->titleAttribute('name'),
-                    Forms\Components\MorphToSelect\Type::make(Street::class)
-                        ->titleAttribute('name'),
-                ]),
-            ]);
+            ->schema(
+                [
+                    Forms\Components\MorphToSelect::make('location')->types(
+                        [
+                            Forms\Components\MorphToSelect\Type::make(Zone::class)->titleAttribute('name'),
+
+                            Forms\Components\MorphToSelect\Type::make(Street::class)->titleAttribute('name'),
+                        ]
+                    )->columns(2),
+
+                    Forms\Components\Select::make('head_id')
+                        ->label('Family head')
+                        ->native(false)
+                        ->options(
+                            fn (?Family $record): array => $record?->members->pluck('name', 'id')->toArray() ?? []
+                        ),
+                ]
+            );
     }
 
     public static function table(Table $table): Table
     {
         return $table
-            ->columns([
-                Tables\Columns\TextColumn::make('location'),
-                Tables\Columns\TextColumn::make('members_count')->label('Total members')->counts('members'),
-            ])
-            ->filters([
-                Tables\Filters\TrashedFilter::make(),
-            ])
-            ->actions([
-                Tables\Actions\ViewAction::make()->iconButton()->color('primary'),
+            ->columns(
+                [
+                    Tables\Columns\TextColumn::make('location'),
 
-                Tables\Actions\EditAction::make()->iconButton()->color('primary'),
+                    Tables\Columns\TextColumn::make('members_count')->label('Total members')->counts('members'),
+                ]
+            )
+            ->filters(
+                [
+                    Tables\Filters\TrashedFilter::make(),
+                ]
+            )
+            ->actions(
+                [
+                    Tables\Actions\ViewAction::make()->iconButton()->color('primary'),
 
-            ], Tables\Enums\ActionsPosition::BeforeColumns)
-            ->bulkActions([
-                TableExportBulkAction::make(),
-                Tables\Actions\RestoreBulkAction::make(),
-                Tables\Actions\DeleteBulkAction::make(),
-            ]);
+                    Tables\Actions\EditAction::make()->iconButton()->color('primary'),
+                ],
+                Tables\Enums\ActionsPosition::BeforeColumns
+            )
+            ->bulkActions(
+                [
+                    TableExportBulkAction::make(),
+
+                    Tables\Actions\RestoreBulkAction::make(),
+
+                    Tables\Actions\DeleteBulkAction::make(),
+                ]
+            );
     }
 
     public static function getEloquentQuery(): Builder
     {
-        return parent::getEloquentQuery()
-            ->withoutGlobalScopes([
-                SoftDeletingScope::class,
-            ]);
+        return parent::getEloquentQuery()->withoutGlobalScope(SoftDeletingScope::class);
     }
 
     public static function getRelations(): array
