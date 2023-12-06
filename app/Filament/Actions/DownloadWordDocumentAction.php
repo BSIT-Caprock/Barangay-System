9e20a9aa8ef0375243b56bf4dddb71294c8ef564
jmarkfen <119049235+jmarkfen@users.noreply.github.com>
2023-12-06 09:44:41 +0800

    Create DownloadWordDocumentAction.php

---
+++ app/Filament/Actions/DownloadWordDocumentAction.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace App\Filament\Actions;
+
+use App\Contracts\DownloadableDocument;
+use Filament\Tables\Actions\Action;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Support\Facades\Storage;
+use PhpOffice\PhpWord\TemplateProcessor;
+
+class DownloadWordDocumentAction extends Action
+{
+    protected $template = null;
+
+    protected $data = null;
+
+    public static function make(?string $name = null): static
+    {
+        return parent::make($name)
+            ->icon('heroicon-s-document-arrow-down')
+            ->action(function (DownloadableDocument $record) {
+                $processor = new TemplateProcessor($record->getTemplate());
+                $processor->setValues($record->getTemplateValues());
+                $filename = $record->getFilename();
+                $filepath = Storage::path($filename);
+                $processor->saveAs($filepath);
+                return response()->download($filepath, $filename)->deleteFileAfterSend();
+            });
+    }
+}
