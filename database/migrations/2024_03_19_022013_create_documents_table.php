e8bd454f49d3e0b2741b7ace86853c019bde53e6
jmarkfen <119049235+jmarkfen@users.noreply.github.com>
2024-03-21 09:27:04 +0800

    make:migration create_documents_table

---
+++ database/migrations/2024_03_19_022013_create_documents_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('documents', function (Blueprint $table) {
+            $table->id();
+            $table->string('filename');
+            $table->string('filepath');
+            $table->timestamps();
+        });
+
+        Schema::create('document_outputs', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('document_id')->constrained()->cascadeOnDelete();
+            $table->json('data');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('document_outputs');
+        Schema::dropIfExists('documents');
+    }
+};
