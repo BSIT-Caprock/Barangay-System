933ce7bc56171767e575d693b668efdb7a5f6ce6
jmarkfen <119049235+jmarkfen@users.noreply.github.com>
2024-03-21 11:31:52 +0800

    remove trace of BarangaySeeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -13,7 +13,6 @@ class DatabaseSeeder extends Seeder
     public function run(): void
     {
         $this->call([
-            BarangaySeeder::class,
             BirthPlaceSeeder::class,
             SexSeeder::class,
             CivilStatusSeeder::class,
