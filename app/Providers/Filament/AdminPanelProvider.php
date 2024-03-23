08ef5874a76a7d33650c0a6a66927d8d149252b7
jmarkfen <119049235+jmarkfen@users.noreply.github.com>
2024-03-23 17:52:00 +0800

    use single-page mode

--- app/Providers/Filament/AdminPanelProvider.php
+++ app/Providers/Filament/AdminPanelProvider.php
@@ -36,6 +36,7 @@ class AdminPanelProvider extends PanelProvider
     {
         return $panel
             ->default()
+            ->spa()
             ->id('admin')
             ->brandName(env('BRGY_FULL'))
             ->path('/')
