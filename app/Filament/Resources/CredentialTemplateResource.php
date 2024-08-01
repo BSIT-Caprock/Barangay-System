bb1b803131a54dcc7e18cbf007598f5cc12eb2f5
Jaymark Feniza <119049235+jmarkfen@users.noreply.github.com>
2024-08-01 13:22:03 +0800

    Credential templates under Credentials

--- app/Filament/Resources/CredentialTemplateResource.php
+++ app/Filament/Resources/CredentialTemplateResource.php
@@ -18,6 +18,8 @@ class CredentialTemplateResource extends Resource
 {
     protected static ?string $model = CredentialTemplate::class;
 
+    protected static ?string $navigationParentItem = 'Credentials';
+
     protected static ?string $navigationIcon = 'heroicon-o-rectangle-stack';
 
     protected static ?string $recordTitleAttribute = 'title';
