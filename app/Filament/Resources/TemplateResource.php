8a1b3e8b989b3ef8fce87f1fc3f24ab0c7b33688
Jaymark Feniza <119049235+jmarkfen@users.noreply.github.com>
2024-08-08 18:10:33 +0800

    make filament resource for templates

---
+++ app/Filament/Resources/TemplateResource.php
@@ -0,0 +1,100 @@
+<?php
+
+namespace App\Filament\Resources;
+
+use App\Filament\Resources\TemplateResource\Pages;
+use App\Filament\Resources\TemplateResource\RelationManagers;
+use App\Models\Generator\Template;
+use Filament\Forms;
+use Filament\Forms\Form;
+use Filament\Resources\Resource;
+use Filament\Tables;
+use Filament\Tables\Table;
+use Illuminate\Database\Eloquent\Builder;
+use Illuminate\Database\Eloquent\SoftDeletingScope;
+use Livewire\Features\SupportFileUploads\TemporaryUploadedFile;
+
+class TemplateResource extends Resource
+{
+    protected static ?string $model = Template::class;
+
+    protected static ?string $navigationIcon = 'heroicon-o-rectangle-stack';
+
+    protected static ?string $navigationParentItem = 'Generator';
+
+    public static function form(Form $form): Form
+    {
+        return $form
+            ->schema([
+                Forms\Components\FileUpload::make('file_path')
+                    ->required()
+                    ->label('File')
+                    ->disk(Template::$disk)
+                    ->directory('templates')
+                    ->storeFileNamesIn('file_name')
+                    ->downloadable()
+                    ->afterStateUpdated(static function (TemporaryUploadedFile $state, Forms\Get $get, Forms\Set $set, string $operation) {
+                        if ($operation == 'create' && blank($get('title'))) {
+                            $set('title', pathinfo($state->getClientOriginalName(), PATHINFO_FILENAME));
+                        }
+                    }),
+                Forms\Components\TextInput::make('title')
+                    ->required(),
+            ]);
+    }
+
+    public static function table(Table $table): Table
+    {
+        return $table
+            ->columns([
+                Tables\Columns\TextColumn::make('title')
+                    ->searchable(),
+                Tables\Columns\TextColumn::make('created_at')
+                    ->dateTime()
+                    ->toggleable(isToggledHiddenByDefault: true),
+                Tables\Columns\TextColumn::make('updated_at')
+                    ->dateTime()
+                    ->toggleable(isToggledHiddenByDefault: true),
+                Tables\Columns\TextColumn::make('deleted_at')
+                    ->dateTime()
+                    ->toggleable(isToggledHiddenByDefault: true),
+            ])
+            ->filters([
+                Tables\Filters\TrashedFilter::make(),
+            ])
+            ->actions([
+                Tables\Actions\EditAction::make(),
+            ])
+            ->bulkActions([
+                Tables\Actions\BulkActionGroup::make([
+                    Tables\Actions\DeleteBulkAction::make(),
+                    Tables\Actions\ForceDeleteBulkAction::make(),
+                    Tables\Actions\RestoreBulkAction::make(),
+                ]),
+            ]);
+    }
+
+    public static function getRelations(): array
+    {
+        return [
+            //
+        ];
+    }
+
+    public static function getPages(): array
+    {
+        return [
+            'index' => Pages\ListTemplates::route('/'),
+            'create' => Pages\CreateTemplate::route('/create'),
+            'edit' => Pages\EditTemplate::route('/{record}/edit'),
+        ];
+    }
+
+    public static function getEloquentQuery(): Builder
+    {
+        return parent::getEloquentQuery()
+            ->withoutGlobalScopes([
+                SoftDeletingScope::class,
+            ]);
+    }
+}
