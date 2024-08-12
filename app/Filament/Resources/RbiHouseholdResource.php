1f60368c1cd0fbca332c786977a6907de60d99d6
Jaymark Feniza <119049235+jmarkfen@users.noreply.github.com>
2024-08-12 13:59:35 +0800

    Add relation manager for household inhabitants

--- app/Filament/Resources/RbiHouseholdResource.php
+++ app/Filament/Resources/RbiHouseholdResource.php
@@ -66,7 +66,7 @@ class RbiHouseholdResource extends Resource
     public static function getRelations(): array
     {
         return [
-            //
+            RelationManagers\InhabitantsRelationManager::class,
         ];
     }
 
---
+++ app/Filament/Resources/RbiHouseholdResource/RelationManagers/InhabitantsRelationManager.php
@@ -0,0 +1,47 @@
+<?php
+
+namespace App\Filament\Resources\RbiHouseholdResource\RelationManagers;
+
+use App\Filament\Resources\RbiInhabitantResource;
+use App\Models\RbiHousehold;
+use Filament\Forms;
+use Filament\Forms\Form;
+use Filament\Resources\RelationManagers\RelationManager;
+use Filament\Tables;
+use Filament\Tables\Table;
+use Illuminate\Database\Eloquent\Builder;
+use Illuminate\Database\Eloquent\SoftDeletingScope;
+
+class InhabitantsRelationManager extends RelationManager
+{
+    protected static string $relationship = 'inhabitants';
+
+    public function form(Form $form): Form
+    {
+        return RbiInhabitantResource::form($form);
+    }
+
+    public function table(Table $table): Table
+    {
+        return RbiInhabitantResource::table($table)
+            ->modelLabel(RbiInhabitantResource::getModelLabel())
+            ->recordTitle(fn ($record) => "{$record->first_name} {$record->middle_name} {$record->last_name} {$record->extension_name} (born {$record->birthdate->format('F j, Y')})")
+            ->inverseRelationship('household')
+            ->headerActions([
+                Tables\Actions\CreateAction::make(),
+                Tables\Actions\AssociateAction::make()
+                    ->recordSelectSearchColumns(['last_name', 'first_name', 'middle_name', 'extension_name']),
+            ])
+            ->actions([
+                Tables\Actions\EditAction::make(),
+                Tables\Actions\DissociateAction::make(),
+                Tables\Actions\DeleteAction::make(),
+            ])
+            ->bulkActions([
+                Tables\Actions\BulkActionGroup::make([
+                    Tables\Actions\DissociateBulkAction::make(),
+                    Tables\Actions\DeleteBulkAction::make(),
+                ]),
+            ]);
+    }
+}
