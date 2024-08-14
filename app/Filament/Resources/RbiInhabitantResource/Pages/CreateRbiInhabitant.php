b227fcaf5aa38e5e76a3e75a5dad0f65e3e2b604
Jaymark Feniza <119049235+jmarkfen@users.noreply.github.com>
2024-08-14 15:49:52 +0800

    Add table widget to create inhabitant page

--- app/Filament/Resources/RbiInhabitantResource/Pages/CreateRbiInhabitant.php
+++ app/Filament/Resources/RbiInhabitantResource/Pages/CreateRbiInhabitant.php
@@ -2,18 +2,21 @@
 
 namespace App\Filament\Resources\RbiInhabitantResource\Pages;
 
+use App\Concerns\Filament\HasTableWidget;
 use App\Filament\Resources\RbiInhabitantResource;
 use Filament\Actions;
 use Filament\Resources\Pages\CreateRecord;
 
 class CreateRbiInhabitant extends CreateRecord
 {
+    use HasTableWidget;
+
     protected static string $resource = RbiInhabitantResource::class;
 
     protected function getFooterWidgets(): array
     {
         return [
-            RbiInhabitantResource\Widgets\ExistingInhabitants::class,
+            $this->getTableWidget(),
         ];
     }
 
