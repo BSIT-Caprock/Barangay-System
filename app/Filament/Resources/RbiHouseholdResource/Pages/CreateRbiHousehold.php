a9a6b07b6f6ed62e980ee5e3b2cd650342373962
Jaymark Feniza <119049235+jmarkfen@users.noreply.github.com>
2024-08-19 18:56:19 +0800

    Change create household page to become find or create

--- app/Filament/Resources/RbiHouseholdResource/Pages/CreateRbiHousehold.php
+++ app/Filament/Resources/RbiHouseholdResource/Pages/CreateRbiHousehold.php
@@ -2,19 +2,27 @@
 
 namespace App\Filament\Resources\RbiHouseholdResource\Pages;
 
+use App\Filament\Pages\FindOrCreateRecord;
 use App\Filament\Resources\RbiHouseholdResource;
 use App\Filament\Widgets\ResourcePageTableWidget;
 use Filament\Actions;
 use Filament\Resources\Pages\CreateRecord;
 
-class CreateRbiHousehold extends CreateRecord
+class CreateRbiHousehold extends FindOrCreateRecord
 {
     protected static string $resource = RbiHouseholdResource::class;
 
-    protected function getFooterWidgets(): array
+    protected static array $identityFields = [
+        'number',
+    ];
+
+    protected function getWhereClauses(string $fieldName, mixed $value)
     {
-        return [
-            ResourcePageTableWidget::makeWith($this->getResource(), initialWhere: []),
-        ];
+        return match ($fieldName) {
+            'number' => [
+                ['number', 'like', "%{$value}%"],
+            ],
+            default => null,
+        };
     }
 }
