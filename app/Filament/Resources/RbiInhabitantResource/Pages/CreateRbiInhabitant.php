4089e77822ba2f0e0556b2df67a09d265915ba6a
Jaymark Feniza <119049235+jmarkfen@users.noreply.github.com>
2024-08-19 16:58:09 +0800

    Improve create inhabitant page by fully implementing find existing feature

--- app/Filament/Resources/RbiInhabitantResource/Pages/CreateRbiInhabitant.php
+++ app/Filament/Resources/RbiInhabitantResource/Pages/CreateRbiInhabitant.php
@@ -5,58 +5,120 @@ namespace App\Filament\Resources\RbiInhabitantResource\Pages;
 use App\Filament\Resources\RbiInhabitantResource;
 use App\Filament\Widgets\ResourcePageTableWidget;
 use Filament\Actions\Action;
+use Filament\Forms\Form;
 use Filament\Forms\Get;
 use Filament\Resources\Pages\CreateRecord;
 use Livewire\Component;
 
 class CreateRbiInhabitant extends CreateRecord
 {
+    /** @var class-string<\Filament\Resources\Resource> */
     protected static string $resource = RbiInhabitantResource::class;
+    
+    public bool $isContinued = false;
+
+    public function form(Form $form): Form
+    {
+        $fields = $form->getFlatFields(withHidden: true);
+
+        $afterStateUpdated = function () {
+            $this->dispatch(ResourcePageTableWidget::WHERE, $this->getTableWhereClauses());
+        };
+
+        $fields['last_name']
+            ->afterStateUpdated($afterStateUpdated)
+            ->live();
+        $fields['first_name']
+            ->afterStateUpdated($afterStateUpdated)
+            ->live();
+        $fields['middle_name']
+            ->afterStateUpdated($afterStateUpdated)
+            ->live();
+        $fields['extension_name']
+            ->afterStateUpdated($afterStateUpdated)
+            ->live();
+        $fields['birthplace']
+            ->afterStateUpdated($afterStateUpdated)
+            ->live();
+        $fields['birthdate']
+            ->afterStateUpdated($afterStateUpdated)
+            ->live();
+        $fields['sex']
+            ->afterStateUpdated($afterStateUpdated)
+            ->live();
+
+        $fields['house_number']
+            ->visible(fn () => $this->isContinued);
+        $fields['street_name']
+            ->visible(fn () => $this->isContinued);
+        $fields['zone_name']
+            ->visible(fn () => $this->isContinued);
+        $fields['civil_status']
+            ->visible(fn () => $this->isContinued);
+        $fields['citizenship']
+            ->visible(fn () => $this->isContinued);
+        $fields['occupation']
+            ->visible(fn () => $this->isContinued);
+        $fields['rbi_household_id']
+            ->visible(fn () => $this->isContinued);
+
+        return $form;
+    }
+
+    protected function getCreateFormAction(): Action
+    {
+        return Action::make('create')
+            ->label(fn () => $this->isContinued ? 'Create': 'Continue')
+            ->requiresConfirmation(fn (): bool => static::$resource::getEloquentQuery()
+                ->where($this->getTableWhereClauses())->count() > 0
+            )
+            ->modalDescription('Are you sure you want to continue? (There are records similar to your inputs / You have not entered any information)')
+            ->action(function () {
+                $this->isContinued = true;
+            });
+    }
 
     protected function getFooterWidgets(): array
     {
-        return [
-            ResourcePageTableWidget::makeWith($this->getResource(), initialWhere: []),
-        ];
+        $widgets = [];
+        if (!$this->isContinued) {
+            $widgets[] = $this->getResourcePageTableWidget();
+        }
+        return $widgets;
     }
 
-    public static function dispatchCreatePageTableWidget(Component $livewire, Get $get)
+    protected function getResourcePageTableWidget()
     {
-        if (! ($livewire instanceof self)) {
-            return;
-        }
+        return ResourcePageTableWidget::makeWith($this->getResource(), initialWhere: []);
+    }
+
+    protected function getTableWhereClauses()
+    {
         $where = [];
-
-        if (filled($get('last_name'))) {
-            $where[] = ['last_name', 'like', "%{$get('last_name')}%"];
-            $where[] = ['middle_name', 'like', "%{$get('last_name')}%", 'or'];
+        $data = $this->data;
+        if (filled($data['last_name'])) {
+            $where[] = ['last_name', 'like', "%{$data['last_name']}%"];
+            $where[] = ['middle_name', 'like', "%{$data['last_name']}%", 'or'];
         }
-
-        if (filled($get('first_name'))) {
-            $where[] = ['first_name', 'like', "%{$get('first_name')}%"];
+        if (filled($data['first_name'])) {
+            $where[] = ['first_name', 'like', "%{$data['first_name']}%"];
         }
-
-        if (filled($get('middle_name'))) {
-            $where[] = ['middle_name', 'like', "%{$get('last_name')}%"];
-            $where[] = ['last_name', 'like', "%{$get('last_name')}%", 'or'];
+        if (filled($data['middle_name'])) {
+            $where[] = ['middle_name', 'like', "%{$data['last_name']}%"];
+            $where[] = ['last_name', 'like', "%{$data['last_name']}%", 'or'];
         }
-
-        if (filled($get('extension_name'))) {
-            $where[] = ['extension_name', 'like', "%{$get('extension_name')}%"];
+        if (filled($data['extension_name'])) {
+            $where[] = ['extension_name', 'like', "%{$data['extension_name']}%"];
         }
-
-        if (filled($get('birthplace'))) {
-            $where[] = ['birthplace', 'like', "%{$get('birthplace')}%"];
+        if (filled($data['birthplace'])) {
+            $where[] = ['birthplace', 'like', "%{$data['birthplace']}%"];
         }
-
-        if (filled($get('birthdate'))) {
-            $where[] = ['birthdate', '=', $get('birthdate')];
+        if (filled($data['birthdate'])) {
+            $where[] = ['birthdate', '=', $data['birthdate']];
         }
-
-        if (filled($get('sex'))) {
-            $where[] = ['sex', '=', $get('sex')];
+        if (filled($data['sex'])) {
+            $where[] = ['sex', '=', $data['sex']];
         }
-
-        $livewire->dispatch(ResourcePageTableWidget::WHERE, $where);
+        return $where;
     }
 }
