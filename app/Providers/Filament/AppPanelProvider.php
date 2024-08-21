c0aff3178b879906763c7f388e5e4ac1abe8dc4e
Jaymark Feniza <119049235+jmarkfen@users.noreply.github.com>
2024-08-21 10:35:30 +0800

    Change primary color

--- app/Providers/Filament/AppPanelProvider.php
+++ app/Providers/Filament/AppPanelProvider.php
@@ -41,7 +41,7 @@ class AppPanelProvider extends PanelProvider
             ->path('')
             // ->login()
             ->colors([
-                'primary' => Color::Amber,
+                'primary' => Color::Blue,
             ])
             ->discoverResources(in: app_path('Filament/Resources'), for: 'App\\Filament\\Resources')
             ->discoverPages(in: app_path('Filament/Pages'), for: 'App\\Filament\\Pages')
